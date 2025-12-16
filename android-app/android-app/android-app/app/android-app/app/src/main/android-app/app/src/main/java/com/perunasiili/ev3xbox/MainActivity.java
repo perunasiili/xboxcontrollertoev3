2685b08366a88d542ae18a4785a70a7086bbf03c
perunasiili <lahti.arttu14@gmail.com>
2025-12-16 22:22:55 +0200

    Add MainActivity for EV3 Xbox Bridge app

---
+++ android-app/android-app/android-app/app/android-app/app/src/main/android-app/app/src/main/java/com/perunasiili/ev3xbox/MainActivity.java
@@ -0,0 +1,19 @@
+package com.perunasiili.ev3xbox;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setText("EV3 Xbox Bridge\nAPK toimii!");
+        tv.setTextSize(24);
+
+        setContentView(tv);
+    }
+}
